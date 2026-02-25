d76a9346e5a126b5c6816491525dd8faff9bc3c3
アップルパイ <sou.120724.s@gmail.com>
2026-02-25 16:42:35 +0900

    ^_^

--- src/main/java/jp/ngt/ngtlib/gui/GuiTextFieldCustom.java
+++ src/main/java/jp/ngt/ngtlib/gui/GuiTextFieldCustom.java
@@ -11,38 +11,21 @@
  *
  *
  */
+package jp.ngt.ngtlib.gui;import java.util.ArrayList;
 
-/*
- *
- *  * AppleExtended
- *  *
- *  * Original code (c) 2020 anatawa12 and other contributors.
- *  * Modifications (c) 2026 Applepie.
- *  *
- *  * This file is part of AppleExtended, which is a derivative work of fixRTM.
- *  * Both are licensed under the GNU Lesser General Public License version 3.
- *  * See LICENSE.txt in the mod root for full license text.
- *
- *
- */
-
-package jp.ngt.ngtlib.gui;
-import java.util.ArrayList;
-import java.util.List;
-import javax.annotation.Nullable;
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.opengl.GL11;
-import jp.ngt.ngtlib.renderer.NGTTessellator;
+import java.util.List;import javax.annotation.Nullable;import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.GL11;import jp.ngt.ngtlib.renderer.NGTTessellator;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.util.ChatAllowedCharacters;
 import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-@SideOnly(Side.CLIENT)
+import net.minecraftforge.fml.relauncher.SideOnly;@SideOnly(Side.CLIENT)
+
 public class GuiTextFieldCustom extends GuiTextField {
     protected final FontRenderer fontRenderer;
+
     protected String text = "";
     protected int maxStringLength = 32;
     protected int cursorCounter;
@@ -60,7 +43,7 @@ public class GuiTextFieldCustom extends GuiTextField {
     private final GuiScreen screen;
     private final List<String> tips = new ArrayList<>();
     protected boolean isDisplayMode;
-    private TextFieldListener listener;
+    private jp.ngt.ngtlib.gui.GuiTextFieldCustom.TextFieldListner listner;
 
     public GuiTextFieldCustom(int id, FontRenderer par1, int x, int y, int w, int h, GuiScreen pScr) {
         super(id, par1, x, y, w, h);
@@ -73,13 +56,8 @@ public class GuiTextFieldCustom extends GuiTextField {
         this.screen = pScr;
     }
 
-    public void setListener(TextFieldListener par1) {
-        this.listener = par1;
-    }
-
-    public GuiTextFieldCustom addTips(String tip) {
-        this.tips.add(tip);
-        return this;
+    public void setListner(jp.ngt.ngtlib.gui.GuiTextFieldCustom.TextFieldListner par1) {
+        this.listner = par1;
     }
 
     public void setDisplayMode(boolean par1) {
@@ -100,6 +78,7 @@ public class GuiTextFieldCustom extends GuiTextField {
             this.text = par1;
         }
         this.setCursorPositionEnd();
+        this.notifyListener();
     }
 
     @Override
@@ -143,6 +122,7 @@ public class GuiTextFieldCustom extends GuiTextField {
 
         this.text = s1;
         this.moveCursorBy(i - this.selectionEnd + l);
+        this.notifyListener();
     }
 
     @Override
@@ -166,7 +146,6 @@ public class GuiTextFieldCustom extends GuiTextField {
                 int j = flag ? this.cursorPosition + p_146175_1_ : this.cursorPosition;
                 int k = flag ? this.cursorPosition : this.cursorPosition + p_146175_1_;
                 String s = "";
-
                 if (j >= 0) {
                     s = this.text.substring(0, j);
                 }
@@ -182,6 +161,7 @@ public class GuiTextFieldCustom extends GuiTextField {
                 }
             }
         }
+        this.notifyListener();
     }
 
     @Override
@@ -277,8 +257,265 @@ public class GuiTextFieldCustom extends GuiTextField {
                 if (this.isEnabled) {
                     this.writeText(GuiScreen.getClipboardString());
                 }
+                return true;
+
+            case 24:
+                GuiScreen.setClipboardString(this.getSelectedText());
+                if (this.isEnabled) {
+                    this.writeText("");
+                }
+                return true;
+
+            default:
+                switch (code) {
+                    case Keyboard.KEY_BACK:
+                        if (GuiScreen.isCtrlKeyDown()) {
+                            if (this.isEnabled) {
+                                this.deleteWords(-1);
+                            }
+                        } else if (this.isEnabled) {
+                            this.deleteFromCursor(-1);
+                        }
+                        return true;
+
+                    case Keyboard.KEY_HOME:
+                        if (GuiScreen.isShiftKeyDown()) {
+                            this.setSelectionPos(0);
+                        } else {
+                            this.setCursorPositionZero();
+                        }
+                        return true;
+
+                    case Keyboard.KEY_LEFT:
+                        if (GuiScreen.isShiftKeyDown()) {
+                            if (GuiScreen.isCtrlKeyDown()) {
+                                this.setSelectionPos(this.getNthWordFromPos(-1, this.getSelectionEnd()));
+                            } else {
+                                this.setSelectionPos(this.getSelectionEnd() - 1);
+                            }
+                        } else if (GuiScreen.isCtrlKeyDown()) {
+                            this.setCursorPosition(this.getNthWordFromCursor(-1));
+                        } else {
+                            this.moveCursorBy(-1);
+                        }
+                        return true;
+
+                    case Keyboard.KEY_RIGHT:
+                        if (GuiScreen.isShiftKeyDown()) {
+                            if (GuiScreen.isCtrlKeyDown()) {
+                                this.setSelectionPos(this.getNthWordFromPos(1, this.getSelectionEnd()));
+                            } else {
+                                this.setSelectionPos(this.getSelectionEnd() + 1);
+                            }
+                        } else if (GuiScreen.isCtrlKeyDown()) {
+                            this.setCursorPosition(this.getNthWordFromCursor(1));
+                        } else {
+                            this.moveCursorBy(1);
+                        }
+                        return true;
+
+                    case Keyboard.KEY_END:
+                        if (GuiScreen.isShiftKeyDown()) {
+                            this.setSelectionPos(this.text.length());
+                        } else {
+                            this.setCursorPositionEnd();
+                        }
+                        return true;
+
+                    case Keyboard.KEY_DELETE:
+                        if (GuiScreen.isCtrlKeyDown()) {
+                            if (this.isEnabled) {
+                                this.deleteWords(1);
+                            }
+                        } else if (this.isEnabled) {
+                            this.deleteFromCursor(1);
+                        }
+                        return true;
+
+                    default:
+                        if (ChatAllowedCharacters.isAllowedCharacter(word)) {
+                            if (this.isEnabled) {
+                                this.writeText(Character.toString(word));
+                            }
+                            return true;
+                        } else {
+                            return false;
+                        }
+                }
         }
-        return super.textboxKeyTyped(word, code);
+    }
+
+    @Override
+    public boolean mouseClicked(int x, int y, int button) {
+        boolean flag = x >= this.x && x < this.x + this.width && y >= this.y && y < this.y + this.height;
+
+        if (this.canLoseFocus) {
+            this.setFocused(flag);
+        }
+
+        if (this.isFocused && button == 0) {
+            int l = x - this.x;
+
+            if (this.enableBackgroundDrawing) {
+                l -= 4;
+            }
+
+            String s = this.fontRenderer.trimStringToWidth(this.text.substring(this.lineScrollOffset), this.getWidth());
+            this.setCursorPosition(this.fontRenderer.trimStringToWidth(s, l).length() + this.lineScrollOffset);
+
+            if (this.listner != null) {
+                this.listner.onClick();
+            }
+        }
+
+        return flag;
+    }
+
+    public void drawTextBox(int mouseX, int mouseY) {
+        this.drawTextBox();
+
+        boolean hovered = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.width && mouseY < this.y + this.height;
+        if (hovered && !this.tips.isEmpty()) {
+            GuiScreenCustom.drawHoveringTextS(this.tips, mouseX, mouseY, this.screen);
+        }
+
+    }
+
+    @Override
+    public final void drawTextBox() {
+        if (this.isDisplayMode) {
+            this.drawString(this.fontRenderer, this.text, this.x, this.y + (this.height - 8) / 2, this.enabledColor);
+        } else {
+            if (this.getVisible()) {
+                if (this.getEnableBackgroundDrawing()) {
+                    drawRect(this.x - 1, this.y - 1, this.x + this.width + 1, this.y + this.height + 1, 0xFFA0A0A0);
+                    drawRect(this.x, this.y, this.x + this.width, this.y + this.height, 0xFF000000);
+                }
+
+                int color = this.isEnabled ? this.enabledColor : this.disabledColor;
+                int j = this.cursorPosition - this.lineScrollOffset;
+                int k = this.selectionEnd - this.lineScrollOffset;
+                String s = this.fontRenderer.trimStringToWidth(this.text.substring(this.lineScrollOffset), this.getWidth());
+                boolean flag = j >= 0 && j <= s.length();
+                boolean flag1 = this.isFocused && this.cursorCounter / 6 % 2 == 0 && flag;
+                int x = this.enableBackgroundDrawing ? this.x + 4 : this.x;
+                int y = this.enableBackgroundDrawing ? this.y + (this.height - 8) / 2 : this.y;
+                int xEnd = x;
+
+                if (k > s.length()) {
+                    k = s.length();
+                }
+
+                if (s.length() > 0) {
+                    String s1 = flag ? s.substring(0, j) : s;
+                    xEnd = this.fontRenderer.drawStringWithShadow(s1, x, y, color);
+                }
+
+                boolean flag2 = this.cursorPosition < this.text.length() || this.text.length() >= this.getMaxStringLength();
+                int x2 = xEnd;
+
+                if (!flag) {
+                    x2 = j > 0 ? x + this.width : x;
+                } else if (flag2) {
+                    x2 = xEnd - 1;
+                    --xEnd;
+                }
+
+                if (s.length() > 0 && flag && j < s.length()) {
+                    this.fontRenderer.drawStringWithShadow(s.substring(j), xEnd, y, color);
+                }
+
+                if (flag1) {
+                    if (flag2) {
+                        Gui.drawRect(x2, y - 1, x2 + 1, y + 1 + this.fontRenderer.FONT_HEIGHT, 0xFFD0D0D0);
+                    } else {
+                        this.fontRenderer.drawStringWithShadow("_", x2, y, color);
+                    }
+                }
+
+                if (k != j) {
+                    int strW = x + this.fontRenderer.getStringWidth(s.substring(0, k));
+                    this.drawCursorVertical(x2, y - 1, strW - 1, y + 1 + this.fontRenderer.FONT_HEIGHT);
+                }
+            }
+        }
+    }
+
+    private void drawCursorVertical(int par1, int par2, int par3, int par4) {
+        int i1;
+
+        if (par1 < par3) {
+            i1 = par1;
+            par1 = par3;
+            par3 = i1;
+        }
+
+        if (par2 < par4) {
+            i1 = par2;
+            par2 = par4;
+            par4 = i1;
+        }
+
+        if (par3 > this.x + this.width) {
+            par3 = this.x + this.width;
+        }
+
+        if (par1 > this.x + this.width) {
+            par1 = this.x + this.width;
+        }
+
+        GL11.glColor4f(0.0F, 0.0F, 255.0F, 255.0F);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glEnable(GL11.GL_COLOR_LOGIC_OP);
+        GL11.glLogicOp(GL11.GL_OR_REVERSE);
+        NGTTessellator tessellator = NGTTessellator.instance;
+        tessellator.startDrawingQuads();
+        tessellator.addVertex((float) par1, (float) par4, 0.0F);
+        tessellator.addVertex((float) par3, (float) par4, 0.0F);
+        tessellator.addVertex((float) par3, (float) par2, 0.0F);
+        tessellator.addVertex((float) par1, (float) par2, 0.0F);
+        tessellator.draw();
+        GL11.glDisable(GL11.GL_COLOR_LOGIC_OP);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+    }
+
+    @Override
+    public void setMaxStringLength(int p_146203_1_) {
+        this.maxStringLength = p_146203_1_;
+
+        if (this.text.length() > p_146203_1_) {
+            this.text = this.text.substring(0, p_146203_1_);
+        }
+    }
+
+    @Override
+    public int getMaxStringLength() {
+        return this.maxStringLength;
+    }
+
+    @Override
+    public int getCursorPosition() {
+        return this.cursorPosition;
+    }
+
+    @Override
+    public boolean getEnableBackgroundDrawing() {
+        return this.enableBackgroundDrawing;
+    }
+
+    @Override
+    public void setEnableBackgroundDrawing(boolean p_146185_1_) {
+        this.enableBackgroundDrawing = p_146185_1_;
+    }
+
+    @Override
+    public void setTextColor(int p_146193_1_) {
+        this.enabledColor = p_146193_1_;
+    }
+
+    @Override
+    public void setDisabledTextColour(int p_146204_1_) {
+        this.disabledColor = p_146204_1_;
     }
 
     @Override
@@ -289,12 +526,109 @@ public class GuiTextFieldCustom extends GuiTextField {
             if (par1 && !this.isFocused) {
                 this.cursorCounter = 0;
             }
-
             this.isFocused = par1;
         }
     }
 
+    @Override
+    public boolean isFocused() {
+        return this.isFocused;
+    }
+
+    @Override
+    public void setEnabled(boolean p_146184_1_) {
+        this.isEnabled = p_146184_1_;
+    }
+
+    @Override
+    public int getSelectionEnd() {
+        return this.selectionEnd;
+    }
+
+    @Override
+    public int getWidth() {
+        return this.getEnableBackgroundDrawing() ? this.width - 8 : this.width;
+    }
+
+    @Override
+    public void setSelectionPos(int p_146199_1_) {
+        int j = this.text.length();
+
+        if (p_146199_1_ > j) {
+            p_146199_1_ = j;
+        }
+
+        if (p_146199_1_ < 0) {
+            p_146199_1_ = 0;
+        }
+
+        this.selectionEnd = p_146199_1_;
+
+        if (this.fontRenderer != null) {
+            if (this.lineScrollOffset > j) {
+                this.lineScrollOffset = j;
+            }
+
+            int k = this.getWidth();
+            String s = this.fontRenderer.trimStringToWidth(this.text.substring(this.lineScrollOffset), k);
+            int l = s.length() + this.lineScrollOffset;
+
+            if (p_146199_1_ == this.lineScrollOffset) {
+                this.lineScrollOffset -= this.fontRenderer.trimStringToWidth(this.text, k, true).length();
+            }
+
+            if (p_146199_1_ > l) {
+                this.lineScrollOffset += p_146199_1_ - l;
+            } else if (p_146199_1_ <= this.lineScrollOffset) {
+                this.lineScrollOffset -= this.lineScrollOffset - p_146199_1_;
+            }
+
+            if (this.lineScrollOffset < 0) {
+                this.lineScrollOffset = 0;
+            }
+
+            if (this.lineScrollOffset > j) {
+                this.lineScrollOffset = j;
+            }
+        }
+    }
+
+    @Override
+    public void setCanLoseFocus(boolean par1) {
+        this.canLoseFocus = par1;
+    }
+
+    @Override
+    public boolean getVisible() {
+        return this.visible;
+    }
+
+    @Override
+    public void setVisible(boolean par1) {
+        this.visible = par1;
+    }
+
+    public jp.ngt.ngtlib.gui.GuiTextFieldCustom addTips(String par1) {
+        this.tips.add(par1);
+        return this;
+    }
+
+    public interface TextFieldListner {
+        void onClick();
+    }
     public interface TextFieldListener {
-        void onType(GuiTextFieldCustom var1);
+        void onChange(jp.ngt.ngtlib.gui.GuiTextFieldCustom field);
+    }
+
+    private jp.ngt.ngtlib.gui.GuiTextFieldCustom.TextFieldListener listener;
+
+    public void setListener(jp.ngt.ngtlib.gui.GuiTextFieldCustom.TextFieldListener listener) {
+        this.listener = listener;
+    }
+
+    private void notifyListener() {
+        if (this.listener != null) {
+            this.listener.onChange(this);
+        }
     }
 }
